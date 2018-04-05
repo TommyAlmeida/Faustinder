6d59a89526866ba89d2f20a6c42d48feb6283076
Cristiana Gramata <cristiana.gramata@gmail.com>
2018-04-05 17:48:39 +0100

    created view initial view and register view

---
+++ src/main/java/org/academiadecodigo/hackatonfundao/faustinder/views/registerView.java
@@ -0,0 +1,41 @@
+package org.academiadecodigo.hackatonfundao.faustinder.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class registerView implements View {
+
+
+    @FXML
+    private Label passwordNotMatch;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField localizationField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField passwordConfirmationField;
+
+    @FXML
+    private ChoiceBox<?> genderField;
+
+    @FXML
+    private ChoiceBox<?> ageField;
+
+    @FXML
+    private Button registerNextButton;
+
+
+}
