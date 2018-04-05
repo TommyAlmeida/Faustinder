1cd45b24e49ded62d12b43e9e4291a0247e169da
Tigas <tiagomonteirogoncalves@gmail.com>
2018-04-06 00:50:14 +0100

    seeded fetish and user tables

---
+++ src/main/resources/META-INF/seed.sql
@@ -0,0 +1,34 @@
+insert into fetish(name) values('Pony Play');
+insert into fetish(name) values('Shibari');
+insert into fetish(name) values('Sensory Deprivation');
+insert into fetish(name) values('Wax Play');
+insert into fetish(name) values('S/M');
+insert into fetish(name) values('Play Party');
+insert into fetish(name) values('Munch');
+insert into fetish(name) values('Master/Slave');
+insert into fetish(name) values('Dungeon Master');
+insert into fetish(name) values('Ageplay');
+
+
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2018-03-06 13:13:10', 'Rosetta', 'M', 'Ab', 'fu0ISdCpgj', '509-842-2797', 'ajosskoviz0');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-11-12 19:51:28', 'Kingscourt', 'M', 'Ewen', 'H5xx4COn0w', '251-162-6687', 'efelkin1');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-09-20 23:41:09', 'Jiangluo', 'M', 'Olav', 'vqutTytQIdQ', '638-838-0484', 'omoule2');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-04-22 07:17:14', 'Limon', 'M', 'Sheffy', 'Nk0cm9jpLM', '476-104-4469', 'shamshar3');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-08-25 23:51:07', 'Sidu', 'M', 'Moshe', '04i2eQ', '733-301-7658', 'mkivelle4');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-12-18 01:21:06', 'Ţāqchah Khānah', 'F', 'Charmaine', 'sQOilF5', '223-181-7714',
+'cjohanssen5');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-12-10 21:48:06', 'Suwaduk', 'M', 'Sergio', 'AWgt528Kg', '114-436-8536', 'sdobney6');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-08-29 20:49:04', 'Palhoça', 'M', 'Algernon', 'eksGL2FG6Q', '838-175-3447', 'abettley7');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-10-04 05:50:49', 'Le Mans', 'M', 'Thom', 'cvAKsj8a', '257-683-5877', 'tfawdery8');
+insert into users (birthDate, city, gender, name, password, phoneNumber, username) values
+('2017-05-09 18:45:03', 'Ząbkowice Śląskie', 'M', 'Marijn', 'QoF7Jle0pJI', '302-991-3250',
+'mkivelhan9');
