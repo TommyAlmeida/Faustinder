b65c7bf1f216005b5a97f9e6a9c3289d4893777e
Cristiana Gramata <cristiana.gramata@gmail.com>
2018-04-05 19:49:00 +0100

    created searching matches view

---
+++ src/main/java/org/academiadecodigo/hackatonfundao/faustinder/views/SearchingMatchesView.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.hackatonfundao.faustinder.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+public class SearchingMatchesView implements View {
+
+        @FXML
+        private Label searchingMatchesText;
+
+        @FXML
+        private ProgressBar progressBar;
+
+    }
+
