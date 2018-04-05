5d54eb00ff02337d385596227fa5700112048924
Cristiana Gramata <cristiana.gramata@gmail.com>
2018-04-05 21:06:01 +0100

    created fetish editor view

---
+++ src/main/java/org/academiadecodigo/hackatonfundao/faustinder/views/FetishEditor.java
@@ -0,0 +1,30 @@
+package org.academiadecodigo.hackatonfundao.faustinder.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+
+public class FetishEditor implements View {
+
+    @FXML
+    private Label fetishSettingText;
+
+    @FXML
+    private Button fetishDoneButton;
+
+    @FXML
+    private CheckBox fetish1;
+
+    @FXML
+    private CheckBox fetish2;
+
+    @FXML
+    private CheckBox fetish3;
+
+    @FXML
+    private Label fetishMaxWarning;
+
+}
+
