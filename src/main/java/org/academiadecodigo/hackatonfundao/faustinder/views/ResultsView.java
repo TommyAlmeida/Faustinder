da8f0a82163513ba5aebd5396e39eea273b92903
Cristiana Gramata <cristiana.gramata@gmail.com>
2018-04-05 20:19:01 +0100

    created results view

---
+++ src/main/java/org/academiadecodigo/hackatonfundao/faustinder/views/ResultsView.java
@@ -0,0 +1,68 @@
+package org.academiadecodigo.hackatonfundao.faustinder.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ResultsView implements View {
+
+    @FXML
+    private Button backButtonResult;
+
+    @FXML
+    private ImageView photoResultOne;
+
+    @FXML
+    private Label usernameResultOne;
+
+    @FXML
+    private Label ageResultOne;
+
+    @FXML
+    private Label cityResultOne;
+
+    @FXML
+    private Label fetishResultOne;
+
+    @FXML
+    private Button connectResultOne;
+
+    @FXML
+    private ImageView photoResultTwo;
+
+    @FXML
+    private ImageView photoResultThree;
+
+    @FXML
+    private Label usernameResultTwo;
+
+    @FXML
+    private Label usernameResultThree;
+
+    @FXML
+    private Label ageResultTwo;
+
+    @FXML
+    private Label ageResultThree;
+
+    @FXML
+    private Label cityResultTwo;
+
+    @FXML
+    private Label cityResultThree;
+
+    @FXML
+    private Label fetishResultTwo;
+
+    @FXML
+    private Label fetishResultThree;
+
+    @FXML
+    private Button connectResultTwo;
+
+    @FXML
+    private Button connectResultThree;
+
+}
+
