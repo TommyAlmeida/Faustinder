f7247d02ca12384d55221786a872618057c624d4
Cristiana Gramata <cristiana.gramata@gmail.com>
2018-04-05 21:41:51 +0100

    added navigation and created the interface for the controllers

---
+++ src/main/java/org/academiadecodigo/hackatonfundao/faustinder/controllers/Controller.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.hackatonfundao.faustinder.controllers;
+
+import org.academiadecodigo.hackatonfundao.faustinder.views.View;
+
+public interface Controller {
+
+
+
+
+
+
+}
