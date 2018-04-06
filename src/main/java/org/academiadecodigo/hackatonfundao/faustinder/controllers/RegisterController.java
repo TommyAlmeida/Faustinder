13c938e723b5b4d55e0d576f6843376e4bfa74bf
Cristiana Gramata <cristiana.gramata@gmail.com>
2018-04-06 05:25:50 +0100

    full registration complete

--- src/main/java/org/academiadecodigo/hackatonfundao/faustinder/controllers/RegisterController.java
+++ src/main/java/org/academiadecodigo/hackatonfundao/faustinder/controllers/RegisterController.java
@@ -9,59 +9,7 @@ import org.academiadecodigo.hackatonfundao.faustinder.helpers.Views;
 public class RegisterController implements Controller {
 
 
-    @FXML
-    private Label fetishSettingText;
 
-    @FXML
-    private CheckBox ponyPlay;
-
-    @FXML
-    private CheckBox shibari;
-
-    @FXML
-    private CheckBox sensoryDeprivation;
-
-    @FXML
-    private CheckBox waxPlay;
-
-    @FXML
-    private CheckBox sM;
-
-    @FXML
-    private CheckBox playParty;
-
-    @FXML
-    private CheckBox ageplay;
-
-    @FXML
-    private CheckBox munch;
-
-    @FXML
-    private CheckBox masterSlave;
-
-    @FXML
-    private CheckBox dungeonMaster;
-
-    @FXML
-    private CheckBox veganBukkake;
-
-    @FXML
-    private CheckBox shoeLicking;
-
-    @FXML
-    private CheckBox feeding;
-
-    @FXML
-    private CheckBox stomping;
-
-    @FXML
-    private CheckBox furry;
-
-    @FXML
-    private Button fetishDoneButton;
-
-    @FXML
-    private Label fetishMaxWarning;
 
     @Override
     public String getName() {
@@ -69,7 +17,6 @@ public class RegisterController implements Controller {
     }
 
     public void doNext(ActionEvent actionEvent) {
-
         Navigation.getInstance().loadScreen(Views.FETISH_SELECTOR.getView());
     }
 
@@ -77,5 +24,7 @@ public class RegisterController implements Controller {
 
         Navigation.getInstance().loadScreen(Views.INITIAL_VIEW.getView());
     }
+
+
 
 }
